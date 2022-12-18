a9530068e7147b6c3e08eeb6f0275bacb4a955dd
wujiabang <wujiabang@actionsky.com>
2022-12-18 18:32:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Usage
+
+pdfs2book can be useful when you want to download all pdfs of a webpage and merge all of them.
+for example, using the following command to download the CS251 course slides as a book:
+
+```
+python3 pdf.py https://www.cs.purdue.edu/homes/ayg/CS251/slides ./CS251
+```
+
+## Requirements
+
+argparse
+urllib
+requests
+wget
+PyPDF2
+python >= 3.5
---
+++ pdf.py
@@ -0,0 +1,66 @@
+import re
+import os
+import argparse
+import ssl
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.parse import urlparse
+from PyPDF2 import PdfMerger
+
+'''
+argparse to enable finicky command-line args
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument('website_url')
+parser.add_argument('file_path')
+parser.add_argument('relative_url', nargs='?', default='none')
+args = parser.parse_args()
+
+# HTML Parse URL
+url_link = str(args.website_url)
+# wget URL
+wget_link = str(args.relative_url)
+# system path
+sys_path = str(args.file_path)
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+website = urlopen(url_link, context=ctx)
+
+# Decode website into string string
+soup = BeautifulSoup(website.read(), features="html.parser")
+links = []
+local_links = []
+divs = soup.find_all('a', attrs={'href': re.compile(".pdf$")})
+for div in divs:
+    href = div.get('href')
+    if href.find("http") == -1:
+        local_links.append(href)
+    else:
+        links.append(href)
+
+# Download non-hosted PDFs
+for link in links:
+    command = 'wget'
+    os.system("%s %s -P %s" % (command, link, sys_path))
+
+# Download local/hosted PDFs
+pdfNames = []
+for link in local_links:
+    command = 'wget'
+    filename = last = link.rsplit('/', 1)[-1]
+    if(wget_link == 'none'):
+        os.system("%s '%s/%s' -P %s" % (command, url_link, link, sys_path))
+    else:
+        os.system("%s '%s/%s' -P %s" % (command, wget_link, link, sys_path))
+    p = os.path.abspath(sys_path)
+    pdfNames.append(os.path.join(p, filename))
+
+merger = PdfMerger()
+for pdf in pdfNames:
+    merger.append(pdf)
+
+merger.write("merged-pdf.pdf")
+merger.close()
