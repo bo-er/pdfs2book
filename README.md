518e82c56fbe6a1a10d9baa89aab2f0e398527dc
wujiabang <wujiabang@actionsky.com>
2022-12-24 22:34:35 +0800

    add prefix and suffix flag

--- README.md
+++ README.md
@@ -4,14 +4,34 @@ pdfs2book can be useful when you want to download all pdfs of a webpage and merg
 for example, using the following command to download the CS251 course slides as a book:
 
 ```
-python3 pdf.py https://www.cs.purdue.edu/homes/ayg/CS251/slides ./CS251
+python3 pdf.py -w https://www.cs.purdue.edu/homes/ayg/CS251/slides -o ./CS251
+```
+### Tip
+using `-p`/`-s` to specify prefix/suffix of the pdf files you want.
+
+### Flags
+
+```
+usage: pdf [-h] [-w WEBSITE_URL] [-o OUTPUT] [-p PREFIX] [-s SUFFIX]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WEBSITE_URL, --website_url WEBSITE_URL
+                        a website that contains multiple links to pdf files
+  -o OUTPUT, --output OUTPUT
+                        directory to save the downloaded pdf files
+  -p PREFIX, --prefix PREFIX
+                        specify pdf filename prefix
+  -s SUFFIX, --suffix SUFFIX
+                        specify pdf filename suffix
+
 ```
 
 ## Requirements
 
-argparse
-urllib
-requests
-wget
-PyPDF2
-python >= 3.5
+- argparse
+- urllib
+- requests
+- wget
+- PyPDF2
+- python >= 3.5
--- pdf.py
+++ pdf.py
@@ -1,27 +1,32 @@
 import re
 import os
-import argparse
 import ssl
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import urlparse
 from PyPDF2 import PdfMerger
+import argparse
 
 '''
 argparse to enable finicky command-line args
 '''
 parser = argparse.ArgumentParser()
-parser.add_argument('website_url')
-parser.add_argument('file_path')
-parser.add_argument('relative_url', nargs='?', default='none')
+parser.add_argument('-w', '--website_url',
+                    help='a website that contains multiple links to pdf files')
+parser.add_argument(
+    '-o', '--output', help='directory to save the downloaded pdf files')
+parser.add_argument('-p', '--prefix', default='none',
+                    help='specify pdf filename prefix')
+parser.add_argument('-s', '--suffix', default='none',
+                    help='specify pdf filename suffix')
 args = parser.parse_args()
 
 # HTML Parse URL
 url_link = str(args.website_url)
-# wget URL
-wget_link = str(args.relative_url)
-# system path
-sys_path = str(args.file_path)
+# directory to save downloaded pdf files
+sys_path = str(args.output)
+prefix = str(args.prefix)
+suffix = str(args.suffix)
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -49,12 +54,13 @@ for link in links:
 # Download local/hosted PDFs
 pdfNames = []
 for link in local_links:
+    if not prefix == 'none' and not link.startswith(prefix):
+        continue
+    if not suffix == 'none' and not link.endwith(prefix):
+        continue
     command = 'wget'
     filename = last = link.rsplit('/', 1)[-1]
-    if(wget_link == 'none'):
-        os.system("%s '%s/%s' -P %s" % (command, url_link, link, sys_path))
-    else:
-        os.system("%s '%s/%s' -P %s" % (command, wget_link, link, sys_path))
+    os.system("%s -nc '%s/%s' -P %s" % (command, url_link, link, sys_path))
     p = os.path.abspath(sys_path)
     pdfNames.append(os.path.join(p, filename))
 
